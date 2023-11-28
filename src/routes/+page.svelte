2411b93262716402a1bcf7264652383872c7da65
Joel Uzcategui <joel@tianguis-mx.com>
2023-11-28 13:25:51 -0400

    navbar done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,25 @@
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-</h1>
+<!-- Nav Container -->
+<nav class="relative container mx-auto p-6">
+    <!-- Flex Container for all Items -->
+    <div class="flex items-center justify-between">
+        <!-- Flex Container for Logo/Menu -->
+        <div class="flex items-center space-x-20">
+            <!-- Logo -->
+            <img src="images/logo.svg" alt="">
+            <!-- Left Menu -->
+            <div class="hidden space-x-8 font-bold lg:flex">
+                <a href="#" class="text-grayishViolet hover:text-veryDarkViolet">Features</a>
+                <a href="#" class="text-grayishViolet hover:text-veryDarkViolet">Pricing</a>
+                <a href="#" class="text-grayishViolet hover:text-veryDarkViolet">Resources</a>
+            </div>
+            
+        </div>
+        <!-- Right Buttons Menu -->
+        <div class="hidden items-center space-x-6 font-bold text-grayishViolet lg:flex">
+            <div class="hover:text-veryDarkViolet">Login</div>
+            <a href="#" class="px-8 py-3 font-bold text-white bg-cyan rounded-full hover:opacity-70">Sign Up</a>
+        </div>
+        <!-- TODO: Hamburger Menu -->
+    </div>
+    <!-- TODO: Mobile Menu -->
+</nav>
