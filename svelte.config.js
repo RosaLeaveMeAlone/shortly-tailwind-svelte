2578f36d50a712fe92a12acafdbe49fbe59b022e
Joel Uzcategui <joel@tianguis-mx.com>
2023-11-29 13:08:51 -0400

    Dockerized

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
-	  },
-	  preprocess: vitePreprocess()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true
+		})
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
